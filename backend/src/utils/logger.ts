bf06c7b0bb058405f3144f36afa17e674ee58280
omg0014 <omg0014@users.noreply.github.com>
2026-05-17 14:54:14 +0530

    Add winston logger

---
+++ backend/src/utils/logger.ts
@@ -0,0 +1,13 @@
+import winston from 'winston';
+
+export const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+  ),
+  defaultMeta: { service: 'vehiclescope' },
+  transports: [
+    new winston.transports.Console(),
+  ],
+});
